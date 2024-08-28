020413aca65adbab52e8e17b6dbefcee527852a2
bmaio <daigoro86dev@gmail.com>
2024-08-28 14:55:58 +0100

    chore: removed unused file

--- src/Common/Core/GenericConstructors.ts
+++
@@ -1,4 +0,0 @@
-import { APIRequestContext, Page } from '@playwright/test';
-
-export type GenericPageConstructor<T> = { new(page: Page): T };
-export type GenericApiClientConstructor<T> = { new(apiRequestContext: APIRequestContext): T };
