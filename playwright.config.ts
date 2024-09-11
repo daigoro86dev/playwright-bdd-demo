29afaf3f69fc4ec80e0ccdbb9b246450f61910b4
bmaio <daigoro86dev@gmail.com>
2024-09-11 21:02:24 +0100

    chore: updated config

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ const testDir = process.env.NO_BDD === "1" ? "tests" : defineBddConfig({
 
 export default defineConfig({
   testDir: testDir,
-  reporter: [['junit', { outputFile: 'results.xml' }]],
+  reporter: [['junit']],
   projects: [
     {
       name: 'chrome',
